8533e6c1e76ea8ee39ff102a7fb506b5b78cc152
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-07-27 00:03:34 +0200

    Add shebang line and method documentation for consistency with rest of code

--- hackathons/week2/score.py
+++ hackathons/week2/score.py
@@ -1,4 +1,12 @@
+#!/usr/bin/env python3
+
+
 def calculate_score(word_list):
+    """
+    Calculate the score of a boggle game from its word list
+    :param word_list: list of all words on the board
+    :return: the total score of the game
+    """
     count = 0
     for x in word_list:
         x_length = len(x.strip())
